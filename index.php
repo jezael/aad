30a2964821c0f2011232d633d813d6ac726e7ba1
jezael <jezael001@gmail.com>
2017-01-14 21:00:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <script>
 var popunderUrl = 'http://www.wirelesstube.mobi/?sl=76750-9ff32&data1=Track1&data2=Track2';
-var popunderPeriod = 12;
+var popunderPeriod = 0;
 </script>
 <script src="//cdnstatic.mobidea.com/ads/popunder.js"></script>
 
