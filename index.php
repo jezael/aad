f301a6ed06ec44649ff1cab23cca663509e99c9c
jezael <jezael001@gmail.com>
2017-01-14 21:04:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ var popunderPeriod = 0;
 </script>
 <script src="//cdnstatic.mobidea.com/ads/popunder.js"></script>
 
+<script>
+var moOverlayUrl = 'http://www.mobilesexlife.mobi/?sl=50120-f097a&data1=Track1&data2=Track2';
+var moOverlayPeriod = 12;
+</script>
+<script src="//cdnstatic.mobidea.com/ads/overlay.js"></script>
+
 <center>
 <img src="http://whos.amung.us/widget/3bhuob1xl5su"//>
 </center>
